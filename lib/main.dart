17a6fd13344c4948b76c5c909bf0e80162177eac
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2023-11-20 22:04:03 +0900

    feat: geolocator 패키지 설치 및 현 위치 불러오기 기능 구현 중
    feat: geolocator 패키지 설치 및 현 위치 불러오기 기능 구현 중

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
+import 'package:flutter/material.dart';
 import 'package:ddasigae_flutter/screens/community_screen.dart';
 import 'package:ddasigae_flutter/screens/notification_screen.dart';
 import 'package:ddasigae_flutter/screens/profile_screen.dart';
-import 'package:flutter/material.dart';
-import 'screens/home_screen.dart';
+import 'package:ddasigae_flutter/screens/home_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -27,11 +27,12 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       title: '따시개',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.yellow,
       ),
       home: Scaffold(
         body: _pages[_currentIndex],
         bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
           currentIndex: _currentIndex,
           onTap: (index) {
             if (mounted) {
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String _currentLocation = '현 위치를 찾는 중...';
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation();
+  }
+
+  Future<void> _getCurrentLocation() async {
+    try {
+      LocationPermission permission = await Geolocator.checkPermission();
+      if (permission == LocationPermission.denied) {
+        permission = await Geolocator.requestPermission();
+        if (permission == LocationPermission.denied) {
+          print('위치 권한이 거부되었습니다.');
+          return;
+        }
+      }
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high);
+      setState(() {
+        _currentLocation = '현재 위치: ${position.latitude}, ${position.longitude}';
+      });
+    } catch (e) {
+      print('위치를 가져오는 데 문제가 발생했습니다: $e');
+      setState(() {
+        _currentLocation = '현 위치를 찾을 수 없습니다.';
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,8 +48,8 @@ class HomeScreen extends StatelessWidget {
         backgroundColor: Colors.white,
         elevation: 0,
       ),
-      body: const Center(
-        child: Text('안녕하세요! 홈 화면입니다.'),
+      body: Center(
+        child: Text(_currentLocation),
       ),
     );
   }
